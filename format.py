88f741d4381bec7a759b9c3780b930d84a5d2e4e
ridwanray <alabarise@gmail.com>
2021-11-10 08:50:49 +0100

    formting array elements

---
+++ format.py
@@ -0,0 +1,24 @@
+[
+    {'Pump': 'Pump1', 'Nozzle': '1', 'Volume': '660.0', 'Price': '160.0', 'Value(N)': '22200.0'}, 
+    {'Pump': 'Pump1', 'Nozzle': '1', 'Volume': '20.0', 'Price': '162.0', 'Value(N)': '200.0'}
+]
+
+
+[
+    {'Pump': 'Pump1', 'Nozzle': '1', 'Volume': '20.0', 'Price': '160.0', 'Value(N)': '800.0'},
+    {'Pump': 'Pump2', 'Nozzle': '2', 'Volume': '20.0', 'Price': '160.0', 'Value(N)': '800.0'}
+]
+
+
+[
+    {'Pump': 'Pump1', 'Nozzle': '1', 'Volume': '20.0', 'Price': '160.0', 'Value(N)': '800.0'}   
+]
+
+'PMS'
+
+[
+    {'Pump': 'Pump2', 'Nozzle': '1', 'Volume': '720.0', 'Price': '160.0', 'Value(N)': '24600.0'},
+    {'Pump': 'Pump1', 'Nozzle': '1', 'Volume': '20.0',  'Price': '160.0', 'Value(N)': '200.0'},
+    {'Pump': '',      'Nozzle': '',  'Volume': '740.0', 'Price': '',      'Value(N)': '24800.0'}
+    
+]
